942f046c63287a507ab3650e1e9b79bdd5c002f0
A Karunaratne <coinkarunaratne@gmail.com>
2023-01-26 02:48:40 +1300

    final-commit

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -11,16 +11,13 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
     className={`relative ${
       active === id ? "lg:flex-[3.5] flex-[10]" : "lg:flex-[0.5] flex-[2]"
     }flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
+    onClick={() => handleClick(id)}
   >
     <img
       src={imgUrl}
       className="absolute w-full h-full object-cover rounded-[24px]"
     />
-    {active === !id ? (
-      <h3 className="font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-20 lg:rotate-[-90deg] lg:origin-[0,0]">
-        {title}
-      </h3>
-    ) : (
+    {active === id ? (
       <div className="absolute bottom-0 p-8 justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px]">
         <div
           className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[24px] glassmorphism mb-[16px]`}
@@ -38,6 +35,10 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
           {title}
         </h2>
       </div>
+    ) : (
+      <h3 className="font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-20 lg:rotate-[-90deg] lg:origin-[0,0]">
+        {title}
+      </h3>
     )}
   </motion.div>
 );
